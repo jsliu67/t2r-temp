9480f8d7b39a6d6e1ac6b5dff2cc88df32f3d81d
Justin Liu <justin789liu@gmail.com>
2022-01-10 21:32:00 -0500

    removed unecessary code

--- header.php
+++ header.php
@@ -7,7 +7,7 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>My Example Website Title</title>
 </head>
-<body>
+<!-- <body>
     <div class="container">
         <div class="navbar">
             <div class="flex nav-flex">
@@ -33,4 +33,4 @@
                 ?>
                 
             </div>
-        </div>
+        </div> -->
--- index.php
+++ index.php
@@ -2,7 +2,7 @@
   get_header();
   require get_template_directory() . '/inc/section_vars.php';
 ?>
-<h1>The Front Page</h1>
+<!-- <h1>The Front Page</h1>
 
 <?php if (get_theme_mod($home_top_img)) { ?>
   <img 
@@ -15,14 +15,14 @@
   <h4>
     <?php echo get_theme_mod($home_top_desc) ?>
   </h4>
-<?php } ?>
+<?php } ?> -->
 
 <!-- Example with default video if $home_top_vid isn't set -->
-<iframe 
+<!-- <iframe 
   class="yt-vid" 
   src="<?php echo get_theme_mod($home_top_vid, 'https://www.youtube.com/embed/A0Wyx-OOX4A'); ?>" 
   frameborder="0" 
   allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen>
-</iframe>
+</iframe> -->
 
 <?php get_footer(); ?>
